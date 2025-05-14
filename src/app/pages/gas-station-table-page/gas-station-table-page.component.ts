d7a34ed7fc06dd8006d937af282a16687ea73b8e
lukts30 <llukas21307@gmail.com>
2025-05-14 20:55:22 +0200

    basic table adress search

--- src/app/pages/gas-station-table-page/gas-station-table-page.component.ts
+++ src/app/pages/gas-station-table-page/gas-station-table-page.component.ts
@@ -4,11 +4,12 @@ import { GasStationDatapoint } from '../../interfaces/gas-station-datapoint';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
 
 
 @Component({
   selector: 'app-gas-station-table-page',
-  imports: [ MatTableModule, MatPaginatorModule, MatSortModule ],
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule],
   templateUrl: './gas-station-table-page.component.html',
   styleUrl: './gas-station-table-page.component.css'
 })
@@ -20,16 +21,26 @@ export class GasStationTablePageComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private gasStationService: GasStationInventoryService) {}
+  constructor(private gasStationService: GasStationInventoryService) { }
 
   ngOnInit(): void {
     this.gasStationService.getGasStationData().subscribe(data => {
       this.dataSource.data = data;
     });
+    this.dataSource.filterPredicate = function (record, filter) {
+      // console.log(`${record.adresse.toLocaleLowerCase()}`);
+      return record.adresse.toLocaleLowerCase().includes(filter.toLocaleLowerCase());
+    }
   }
 
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
+  }
+
+  // https://www.angularjswiki.com/material/mat-table-filter/
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
