5f9f955ea6ba3487c248f1007ef659a17a337c8c
lukts30 <llukas21307@gmail.com>
2025-05-14 15:10:47 +0200

    menu-bar etc.

---
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-menu-bar',
+    imports: [ 
+        CommonModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCardModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatTableModule,
+        RouterModule,
+     ],
+    templateUrl: './menu-bar.component.html',
+    styleUrls: ['./menu-bar.component.css']
+})
+export class MenuBarComponent implements OnInit {
+
+    /*
+    This array holds the definition of the menu's buttons.
+   */
+    buttons = [
+        {title: 'Welcome', routerLink: ''}, // the tile is the text on the button, the routerLink specifies, where it will navigate
+    ];
+
+    constructor() { }
+
+    ngOnInit(): void {
+        // this.fetchUser();
+    }
+}
