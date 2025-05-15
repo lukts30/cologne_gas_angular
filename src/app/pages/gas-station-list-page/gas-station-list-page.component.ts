cce741d268c6b01d26d3c803ccbb659c459fdfd3
lukts30 <llukas21307@gmail.com>
2025-05-15 14:37:31 +0200

    gas-station-list-page: use MatList

--- src/app/pages/gas-station-list-page/gas-station-list-page.component.ts
+++ src/app/pages/gas-station-list-page/gas-station-list-page.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { GasStationInventoryService } from '../../services/gas-station-inventory-service.service';
 import { GasStationDatapoint } from '../../interfaces/gas-station-datapoint';
 import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-gas-station-list-page',
-  imports: [ CommonModule ],
+  imports: [ 
+    CommonModule, 
+    MatListModule,
+    MatProgressSpinnerModule
+  ],
   templateUrl: './gas-station-list-page.component.html',
   styleUrl: './gas-station-list-page.component.css'
 })
